71f16ae04a6628634d053a5775aef978c350a211
Himanshu Agarwal <hagarwal74@gmail.com>
2023-04-05 23:29:36 -0400

    first commit

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,14 @@
+package runners;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        plugin = {"pretty"},
+        features = "src/test/resources/features/Register.feature",
+        tags = "@test1",
+        glue = "stepdefinitions"
+)
+public class Runner {}
